8802e8966925a9247eaf82b046fad72613e1ad1f
Koonny Amaya <konnylizamaya@gmail.com>
2022-02-20 20:30:54 -0600

    bordes

---
+++ resources/views/examples.blade.php
@@ -0,0 +1,43 @@
+<!doctype html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <title>Laravel with Tailwind</title>
+</head>
+
+<body>
+    <div class="container mx-auto pt-3">
+        {{-- <div class="w-64 h-64 bg-blue-300 border-l-4 border-black hover:border-green-700"> --}}
+        <div class="w-64 h-64 bg-blue-300 border-4 border-black hover:border-opacity-25 border-dotted rounded-lg">
+
+        </div>
+
+        <div class="divide-y-8 divide-gray-300 divide-dashed">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur nam maiores iure, magni sed odit debitis
+                id deleniti officiis ullam soluta voluptate. Eveniet reiciendis est deleniti accusamus rem exercitationem
+                debitis.</p>
+            <p>Nobis dolore illo iusto nemo ea, obcaecati facere alias praesentium sint quidem? Voluptatibus corrupti quos
+                laudantium esse similique ducimus quae, ea sunt. Enim magnam perferendis id, ut temporibus eum nulla.</p>
+            <p>Illo ab beatae totam inventore error, ipsum nihil sequi qui, cumque officia eius odio fugiat? Unde aspernatur
+                amet officiis soluta optio autem eius molestias sunt dignissimos eligendi! Accusantium, iusto asperiores.
+            </p>
+            <p>Quisquam impedit suscipit ad laudantium. Dignissimos illum rem sequi, laborum, nulla, cupiditate ratione
+                magni adipisci repellendus numquam quibusdam quod in sapiente tenetur dolores ipsum! Laboriosam tenetur
+                consequuntur tempora quasi fuga!</p>
+        </div>
+
+        <nav class="divide-x-2 divide-blue-400">
+            <a href="">link 1</a>
+            <a href="">link 2</a>
+            <a href="">link 3</a>
+            <a href="">link 4</a>
+            <a href="">link 5</a>
+        </nav>
+        
+    </div>
+</body>
+
+</html>
